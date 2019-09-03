dd7d6a6a50f6eb508a60275a34923b5c2d8ad555
Bishop <lake.d.bishop@gmail.com>
2019-09-03 17:37:36 -0500

    second get request complete

--- index.js
+++ index.js
@@ -59,6 +59,20 @@ server.get('/api/posts', (req, res) => {
         })
 })
 
+server.get('/api/posts/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post.length == 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+})
+
 const port = 8000
 
 server.listen(port, () => console.log(`\n Server listening on port ${port} \n`))
