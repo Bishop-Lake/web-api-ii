d21fe7ee05af5e8ef5a24873d8c1c72dce2e6110
Bishop <lake.d.bishop@gmail.com>
2019-09-03 16:41:21 -0500

    added first post and working on second

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const Posts = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json())
+
+server.post('/api/posts', (request, response) => {
+    const {title, contents} = request.body;
+    
+    if (!title || !contents) {
+        response
+            .status(400)
+            .json({errorMessage: 'Please provide both a title and contents for your post.'})
+    } else {
+        Posts.insert(request.body)
+            .then(post => {
+                response.status(201).json(post)
+            })
+            .catch(() => {
+                response.status(500).json({
+                    error: "There was an error while saving the post to the database"
+                });
+            })
+    }
+})
+
+server.post('/api/posts/:id/comments', (req, res) => {
+    const comment = req.body
+    comment.post_id = req.params.id
+
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (!comment.text) {
+                res.send('yass')
+            }
+        })
+})
+
+const port = 8000
+
+server.listen(port, () => console.log(`\n Server listening on port ${port} \n`))
